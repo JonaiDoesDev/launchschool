ddff821d1cdfc0e3f6897d8e714ba52f26650efe
JonaiLive <jonaisoto.dev@gmail.com>
2022-02-21 21:50:39 -0500

    refactoring

--- lesson_2/rockpaperscissors.rb
+++ lesson_2/rockpaperscissors.rb
@@ -1,58 +1,47 @@
-VALID_CHOICES = ["rock", "paper", "scissors"]
-
-def test_method
-  prompt('test message')
-end
-
-# test_method
-
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
-  puts ("==> #{message}")
+  puts("==> #{message}")
 end
 
-test_method
+def win?(first, second)
+  (first == "rock" && second == "scissors") ||
+    (first == "paper" && second == "rock") ||
+    (first == "scissors" && second == "paper")
+end
 
 def display_results(player, computer)
-  if (player == "rock" && computer == "scissors") ||
-      (player == "paper" && computer == "rock") ||
-      (player == "scissors" && computer == "paper")
+  if win?(player, computer)
     prompt("You Won!")
-  elsif
-    (player == "scissors" && computer == "rock") || 
-    (player == "rock" && computer == "paper") ||
-    (player == "paper" && computer == "scissors")
+  elsif win?(computer, player)
     prompt("You Lost!")
   else
     prompt("It's a tie!")
   end
 end
 
-
   loop do
-  player = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    player = gets.chomp
+    player = ''
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      player = gets.chomp
 
-    if VALID_CHOICES.include?(player)
-      break
-    else
-      prompt("That's not a valid choice.")
+      if VALID_CHOICES.include?(player)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    computer = VALID_CHOICES.sample
+
+    prompt("You chose: #{player.capitalize}
+          The Computer chose: #{computer.capitalize}")
+
+    display_results(player, computer)
+    prompt("Want to play again?")
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
   end
 
-  computer = VALID_CHOICES.sample
-
-  prompt("You chose: #{player.capitalize}. The Computer chose: #{computer.capitalize}")
-
-  display_results(player, computer)
-  
-
-  prompt("Want to play again?")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
-   end
-
- prompt("Goodbye")
-
+prompt("Goodbye")
