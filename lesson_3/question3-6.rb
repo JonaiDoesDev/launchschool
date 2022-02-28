5891b40e66c000f4facc69bc89a0b887421a693c
JonaiLive <jonaisoto.dev@gmail.com>
2022-02-27 21:28:00 -0500

    trying to commit often

--- lesson_3/question3-6.rb
+++ lesson_3/question3-6.rb
@@ -1,13 +1,13 @@
 answer = 42
 
-def mess_with_it(some_number)
-  some_number += 8
-end
+# def mess_with_it(some_number)
+#   some_number += 8
+# end
 
-new_answer = mess_with_it(answer)
+# new_answer = mess_with_it(answer)
 
 p answer - 8
-p new_answer
+# p new_answer
 
 
 
