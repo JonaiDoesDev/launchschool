c6802cab5c15d747bbfdc8b373646e2def784d1b
JonaiLive <jonaisoto.dev@gmail.com>
2022-04-09 19:14:59 -0400

    working on loops

--- lesson_4/quickexample.rb
+++ lesson_4/quickexample.rb
@@ -18,11 +18,27 @@
 #   break if counter > 5
 # end
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# counter = 0
+
+# loop do
+#   break if counter == alphabet.size
+#   puts alphabet[counter]
+#   counter += 1
+# end
+
+number_of_pets = {
+  'dogs' => 2,
+  'cats' => 4,
+  'fish' => 1
+}
+pets = number_of_pets.keys # => ['dogs', 'cats', 'fish']
 counter = 0
 
 loop do
-  break if counter == alphabet.size
-  puts alphabet[counter]
+  break if counter == number_of_pets.size
+  current_pet = pets[counter]
+  current_pet_number = number_of_pets[current_pet]
+  puts "I have #{current_pet_number} #{current_pet}!"
   counter += 1
 end
