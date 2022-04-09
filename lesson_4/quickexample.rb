549c6ab243db63a87423bafeba87601e2a695bc5
JonaiLive <jonaisoto.dev@gmail.com>
2022-04-08 21:34:54 -0400

    learning about loops

--- lesson_4/quickexample.rb
+++ lesson_4/quickexample.rb
@@ -1,5 +1,28 @@
-# arr = [[:name, 'Joe'], [:age, 10], [:favorite_color, 'blue']]
+# loop do
+#   number = rand(1..10)
+#   puts 'Hello!'
+#   if number == 5
+#     puts 'Exiting...'
+#     break
+#   end
+# end
+ 
 
-# puts arr.to_h
 
+# counter = 0
 
+# loop do
+#   counter += 1
+#   next if counter.odd?
+#   puts counter
+#   break if counter > 5
+# end
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+counter = 0
+
+loop do
+  break if counter == alphabet.size
+  puts alphabet[counter]
+  counter += 1
+end
