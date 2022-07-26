3967cadfd6d457ab068255a0a9dfafd8092a7a05
JonaiLive <jonaisoto.dev@gmail.com>
2022-07-25 20:53:40 -0400

    examples

---
+++ lesson_4/selectionandtrasnformation.rb/example4.rb
@@ -0,0 +1,8 @@
+produce = {
+  'apple' => 'Fruit',
+  'carrot' => 'Vegetable',
+  'pear' => 'Fruit',
+  'broccoli' => 'Vegetable'
+}
+
+select_fruit(produce) # => {"apple"=>"Fruit", "pear"=>"Fruit"}
