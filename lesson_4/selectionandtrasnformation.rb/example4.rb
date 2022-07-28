e682820d56028539da5b7915afe920af87a706e7
JonaiLive <jonaisoto.dev@gmail.com>
2022-07-28 00:01:02 -0400

    selection and transformation

--- lesson_4/selectionandtrasnformation.rb/example4.rb
+++ lesson_4/selectionandtrasnformation.rb/example4.rb
@@ -1,21 +1,64 @@
-def select_fruit
-    fruit = {}
+def select_fruit(produce_list)
+    produce_keys = produce_list.keys
     counter = 0
+    selected_fruits = {}
 
     loop do
-      while
+      break if counter == produce_keys.size
 
+      current_key = produce_keys[counter]
+      current_value = produce_list[current_key]
+
+      if current_value == 'Fruit'
+        selected_fruits[current_key] = current_value
+      end
+
+      counter += 1
 end
 
+puts selected_fruits
+end
 
-
-
-
-produce = {
+blahblah = {
   'apple' => 'Fruit',
   'carrot' => 'Vegetable',
   'pear' => 'Fruit',
   'broccoli' => 'Vegetable'
 }
 
-select_fruit(produce) # => {"apple"=>"Fruit", "pear"=>"Fruit"}
+
+
+puts select_fruit(blahblah)
+
+
+# def select_fruit(produce_list)
+#   produce_keys = produce_list.keys
+#   counter = 0
+#   selected_fruits = {}
+
+#   loop do
+#     # this has to be at the top in case produce_list is empty hash
+#     break if counter == produce_keys.size
+
+#     current_key = produce_keys[counter]
+#     current_value = produce_list[current_key]
+
+#     if current_value == 'Fruit'
+#       selected_fruits[current_key] = current_value
+#     end
+
+#     counter += 1
+#   end
+
+#   selected_fruits
+# end
+
+
+# blahblah = {
+#   'apple' => 'Fruit',
+#   'carrot' => 'Vegetable',
+#   'pear' => 'Fruit',
+#   'broccoli' => 'Vegetable'
+# }
+
+# puts select_fruit(blahblah)
