3478c833f47c241391f2fc377770093f9b84d472
JonaiLive <jonaisoto.dev@gmail.com>
2022-07-28 23:18:07 -0400

    methods

--- lesson_4/selectionandtrasnformation.rb/example7.rb
+++ lesson_4/selectionandtrasnformation.rb/example7.rb
@@ -1,12 +1,37 @@
+# def general_select(produce_list, selection_criteria)
+#   produce_keys = produce_list.keys
+#   counter = 0
+#   selected_produce = {}
+
+#   loop do
+#     break if counter == produce_keys.size
+
+#     current_key = produce_keys[counter]
+#     current_value = produce_list[current_key]
+
+#     # used to be current_value == 'Fruit'
+#     if current_value == selection_criteria
+#       selected_produce[current_key] = current_value
+#     end
+
+#     counter += 1
+#   end
+
+#   selected_produce
+# end
+
+
+
+
 def general_select(produce_list, selection_criteria)
-  produce_keys = produce_list.keys
+  # produce_keys = produce_list.keys
   counter = 0
   selected_produce = {}
 
   loop do
-    break if counter == produce_keys.size
+    break if counter == produce_list.keys.size
 
-    current_key = produce_keys[counter]
+    current_key = produce_list.keys[counter]
     current_value = produce_list[current_key]
 
     # used to be current_value == 'Fruit'
@@ -19,3 +44,16 @@ def general_select(produce_list, selection_criteria)
 
   selected_produce
 end
+
+
+
+
+
+produce = {
+  'apple' => 'Fruit',
+  'carrot' => 'Vegetable',
+  'pear' => 'Fruit',
+  'broccoli' => 'Vegetable'
+}
+
+puts general_select(produce, 'Fruit')
