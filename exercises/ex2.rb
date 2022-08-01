4e625aa1ef3527d32f13c577a3d970b5dd9bb603
JonaiLive <jonaisoto.dev@gmail.com>
2022-08-01 14:00:48 -0400

    Working on some exercises

---
+++ exercises/ex2.rb
@@ -0,0 +1,10 @@
+require 'date'
+
+puts Date.civil #This line should return todays date
+puts Date.civil(2016) #This line should return 08/01/2016
+puts Date.civil(2016, 5) #This line should return 05/01/2016 
+puts Date.civil(2016, 5, 13) #This line should return 05/13/2016
+
+
+What will each of the 4 puts statements print?
+
