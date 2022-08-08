db010afecb9b6fa193ec62006c3a2cdb6a2073db
JonaiLive <jonaisoto.dev@gmail.com>
2022-08-07 23:21:20 -0400

    working with data sets

---
+++ lesson_4/additionalpractice/prac3.rb
@@ -0,0 +1,16 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+values = []
+
+
+ages.each_value { |value| values.push(value < 100)}
+
+puts values
+
+
+
+
+# added_up = values.sum
+
+# puts added_up
+
