b31256bd5aa58fb660461dffcddcfe1f1da151fe
Jonai Soto <jonaisoto.dev@gmail.com>
2022-11-06 10:10:26 -0500

    working on a solution

---
+++ smallproblems/easy1/howmany.rb
@@ -0,0 +1,17 @@
+vehicles = [
+    'car', 'car', 'truck', 'car', 'SUV', 'truck',
+    'motorcycle', 'motorcycle', 'car', 'truck'
+  ]
+
+
+
+def test_count(vehicle)
+    index = 0
+    if vehicle[index] == vehicle[index + 1]
+    print "true"
+    else
+        print "false"
+end
+end
+
+print test_count(vehicles)
