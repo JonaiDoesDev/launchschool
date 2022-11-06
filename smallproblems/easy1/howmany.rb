430f9f9441d71775b73be75935cb0467ac07dcee
Jonai Soto <jonaisoto.dev@gmail.com>
2022-11-06 12:44:30 -0500

    could not come up with my own answer

--- smallproblems/easy1/howmany.rb
+++ smallproblems/easy1/howmany.rb
@@ -5,13 +5,32 @@ vehicles = [
 
 
 
-def test_count(vehicle)
-    index = 0
-    if vehicle[index] == vehicle[index + 1]
-    
-    else
-        print "false"
-end
+# def test_count(vehicle)
+#     totals = {}
+#     index = 0
+#     if vehicle[index] == vehicle[index + 1]
+#     print totals.store(index, 3)
+#     index =+ 1
+
+#     else
+#         print "false"
+# end
+# end
+
+# print test_count(vehicles)
+
+
+def count_occurrences(array)
+    occurrences = {}
+
+    array.uniq.each do |element|
+        occurrences[element] = array.count(element)
+    end
+
+    occurrences.each do |element, count|
+        puts "#{element} => #{count}"
+    end    
 end
 
-print test_count(vehicles)
+count_occurrences(vehicles
+)
