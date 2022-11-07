f7cd37d8d5cb776e6a99bf1b7d5ffb1b5866b23c
Jonai Soto <jonaisoto.dev@gmail.com>
2022-11-06 20:00:18 -0500

    Delete exercises directory

--- exercises/ex4.rb
+++
@@ -1,9 +0,0 @@
-# The Array#bsearch method is used to search ordered Arrays more quickly than #find and #select can. Assume you have the following code:
-
-
-a = [1, 4, 8, 11, 15, 19]
-
-this_value = a.bsearch {|x| x > 8}
-
-puts this_value
-
