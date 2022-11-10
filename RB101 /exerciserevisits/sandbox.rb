61e511ba84d6725bd01542302646b159f9f0dfe4
Jonai Soto <jonaisoto.dev@gmail.com>
2022-11-10 03:36:04 -0500

    playing with some examples

--- RB101 /exerciserevisits/sandbox.rb
+++ RB101 /exerciserevisits/sandbox.rb
@@ -1,9 +1,32 @@
-fruits = ['apple', 'banana', 'pear']
-teh_s = []
+# fruits = ['apple', 'banana', 'pear']
+# teh_s = []
 
-fruits.each do |item|
-  item + "s" << teh_s
+# fruits.each do |item|
+#   teh_s << item + "s"
   
+# end
+
+#   print teh_s
+
+
+
+fruits = ['apple', 'banana', 'pear']
+
+
+
+def transform(array)
+  teh_s = []
+  array.each do |item|
+  item + "s" + " "
+  # print mod
+  # teh_s.push(mod)
+  print teh_s
+  end
 end
 
-  print teh_s
+
+
+
+print transform(fruits)
+
+# print fruits
