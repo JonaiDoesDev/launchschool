4f1d1a23dc624b1d00728b9fb3ef635fb8070fbb
Jonai Soto <jonaisoto.dev@gmail.com>
2022-11-12 16:19:44 -0500

    sandbox

--- RB101/exerciserevisits/sandbox.rb
+++ RB101/exerciserevisits/sandbox.rb
@@ -1,27 +1,117 @@
-sentence = "jonai is the man"
+# sentence = "jonai is the man"
 
-def select_vowels(str)
-  selected_chars = ''
-  counter = 0 
+# def select_vowels(str)
+#   selected_chars = ''
+#   counter = 0 
 
-  loop do
-    current_char = str[counter]
+#   loop do
+#     current_char = str[counter]
 
-    if 'aeiouAEIOU'.include?(current_char)
-      selected_chars << current_char
-  end
+#     if 'aeiouAEIOU'.include?(current_char)
+#       selected_chars << current_char
+#   end
 
-  counter += 1
-  break if counter == str.length
+#   counter += 1
+#   break if counter == str.length
+# end
+
+# print selected_chars
+# end
+
+
+
+# sentence = "jonai is the man"
+# number_of_vowels = select_vowels(sentence).size
+# number_of_vowels
+
+# # print number_of_vowels
+
+
+
+
+
+# 4.times do |num|
+#   if num == 0
+#     var = 'first_iteration'
+#     p var
+#   else
+#     p var
+#   end
+# end
+
+
+
+# # ruby
+# def foo
+#   x = x.to_s + 'a'
+#   p x
+# end
+# foo
+
+
+# fruits = ['apple', 'banana', 'pear']
+
+
+# def transform(array)
+#   teh_s = []
+#   array.map do |item|
+#   mod = item + 's'
+#   teh_s << mod
+#   # print teh_s
+#   end
+# end
+
+
+
+
+# print transform(fruits)
+
+
+# def select_vowels(str)
+#   selected_chars = ''
+#   counter = 0
+
+#   loop do
+#     current_char = str[counter]
+
+#     if 'aeiouAEIOU'.include?(current_char)
+#       selected_chars << current_char
+#     end
+
+#     counter += 1
+#     break if counter == str.size
+#   end
+
+#   selected_chars
+# end
+
+# select_vowels('the quick brown fox')      # => "euioo"
+
+# sentence = 'I wandered lonely as a cloud'
+# number_of_volwels = select_vowels(sentence).size                   # => "Iaeeoeaaou"
+
+
+# p number_of_volwels
+
+
+
+
+produce = {
+  'apple' => 'Fruit',
+  'carrot' => 'Vegetable',
+  'pear' => 'Fruit',
+  'broccoli' => 'Vegetable'
+}
+
+# select_fruit(produce) # => {"apple"=>"Fruit", "pear"=>"Fruit"}
+
+
+select_fruit(hash)
+new_hash = {}
+counter = has
+loop do |key, value|
+  current_iteration = hash[]
+  if hash.value? == "fruit"
+
+
 end
-
-print selected_chars
-end
-
-
-
-sentence = "jonai is the man"
-number_of_vowels = select_vowels(sentence).size
-number_of_vowels
-
-# print number_of_vowels
