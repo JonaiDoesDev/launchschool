eba28587e77c1cdacb5d4d41985dac6187833597
Jonai Soto <jonaisoto.dev@gmail.com>
2022-11-22 20:20:01 -0500

    adding some pedac

---
+++ RB101/lesson_4/additionalpractice/prac9.rb
@@ -0,0 +1,33 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+
+
+=begin
+PEDAC
+
+Input: a hash where each entry has its own hash of items
+example: 
+hash = {"Herman" => { "age" => 32, "gender" => "male" }
+
+output: same hash as above but with a new item added to the hash
+example: 
+hash = {"Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult"}
+
+
+
+
+
+
+
+
+
+
+
+
+=end
