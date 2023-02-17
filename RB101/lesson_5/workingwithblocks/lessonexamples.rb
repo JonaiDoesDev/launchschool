cd95d737c11e0300e6a864edb486a305a039c0c6
Jonai Soto <jonaisoto.dev@gmail.com>
2023-02-17 18:53:48 -0500

    working with blocks example 5

--- RB101/lesson_5/workingwithblocks/lessonexamples.rb
+++ RB101/lesson_5/workingwithblocks/lessonexamples.rb
@@ -1,8 +1,18 @@
-# [[1, 2], [3, 4]].each do |arr|
-#   p arr.first
+# # [[1, 2], [3, 4]].each do |arr|
+# #   p arr.first
+# # end
+
+
+# [[1, 2], [3, 4]].map do |arr|
+#   puts arr.first
 # end
+ 
 
-
-[[1, 2], [3, 4]].map do |arr|
-  puts arr.first
+my_arr = [[18, 7], [3, 12]].each do |arr|
+  arr.each do |num|
+    if num > 5
+      puts num
+    end
+  end
 end
+
