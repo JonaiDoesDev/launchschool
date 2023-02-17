8202a1de797c3d970a1347e42b045b7f0213ab6b
Jonai Soto <jonaisoto.dev@gmail.com>
2023-02-16 22:59:50 -0500

    working with blocks

--- RB101/lesson_5/nesteddatastructures/sandbox.rb
+++ RB101/lesson_5/nesteddatastructures/sandbox.rb
@@ -1,6 +1,10 @@
-arr = [['a', ['b']], { b: 'bear', c: 'cat' }, 'cab']
+# arr = [['a', ['b']], { b: 'bear', c: 'cat' }, 'cab']
 
-# something to look into
-arr[2][:a] = 'ant'  
+# # something to look into
+# arr[2][:a] = 'ant'  
 
-p arr
+# p arr
+
+arr = [[1], [2], [3]].freeze
+arr << 4
+arr # => [[1], [2], [3, 4]]
--- RB101/lesson_5/workingwithblocks/lessonexamples.rb
+++ RB101/lesson_5/workingwithblocks/lessonexamples.rb
@@ -1,3 +1,8 @@
-[[1, 2], [3, 4]].each do |arr|
+# [[1, 2], [3, 4]].each do |arr|
+#   p arr.first
+# end
+
+
+[[1, 2], [3, 4]].map do |arr|
   puts arr.first
 end
