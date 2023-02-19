4ca7b57da1b965d72889eb36ef72c8c921159586
Jonai Soto <jonaisoto.dev@gmail.com>
2023-02-19 14:42:40 -0500

    working with blocks

--- RB101/lesson_5/workingwithblocks/sandbox.rb
+++ RB101/lesson_5/workingwithblocks/sandbox.rb
@@ -1,5 +1,15 @@
-[{ a: 'ant', b: 'elephant' }, { c: 'bat', z: 'zebra' }, {d: 'dog', e: "equestrian"}, {f: 'tola', g: 'garfield'}].select do |hash|
-  hash.all? do |key, value|
-    value[0] == key.to_s
+# [{ a: 'ant', b: 'elephant' }, { c: 'bat', z: 'zebra' }, {d: 'dog', e: "equestrian"}, {f: 'tola', g: 'garfield'}].select do |hash|
+#   hash.all? do |key, value|
+#     value[0] == key.to_s
+#   end
+# end
+
+
+
+arr = [['1', '8', '11'], ['2', '6', '13'], ['2', '12', '15'], ['1', '8', '9']]
+
+arr.sort_by do |sub_arr|
+  sub_arr.map do |num|
+    num.to_i
   end
 end
