68a5b9432a1af0fbbcc727a98048419d2112919a
Jonai Soto <jonaisoto.dev@gmail.com>
2023-02-19 16:56:38 -0500

    working with blocks

--- RB101/lesson_5/workingwithblocks/sandbox.rb
+++ RB101/lesson_5/workingwithblocks/sandbox.rb
@@ -6,10 +6,25 @@
 
 
 
-arr = [['1', '8', '11'], ['2', '6', '13'], ['2', '12', '15'], ['1', '8', '9']]
+# arr = [['1', '8', '11'], ['2', '6', '13'], ['2', '12', '15'], ['1', '8', '9']]
 
-arr.sort_by do |sub_arr|
-  sub_arr.map do |num|
-    num.to_i
-  end
+# arr.sort_by do |sub_arr|
+#   sub_arr.map do |num|
+#     num.to_i
+#   end
+# end
+
+
+# [[[1], [2], [3], [4]], [['a'], ['b'], ['c']]].map do |element1|
+#   element1.each do |element2|
+#      element2.partition do |element3|
+#      p element3.size > 0
+#     end
+#   end
+# end
+
+[[[1, 2], [3, 4]], [5, 6]].map do |arr|
+  arr.map do |el|
+    el
+end
 end
