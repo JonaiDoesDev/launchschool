2eda67833266dd775f268cfec8e4fb88a4223b33
Jonai Soto <jonaisoto.dev@gmail.com>
2023-03-06 11:03:40 -0500

    referencing

--- RB101/lesson_5/practiceproblems/pp3.rb
+++ RB101/lesson_5/practiceproblems/pp3.rb
@@ -8,6 +8,12 @@ puts arr2[1][:third][0]
 
 arr3 = [['abc'], ['def'], {third: ['ghi']}]
 
+puts arr3[2][:third][0][0]
+
 hsh1 = {'a' => ['d', 'e'], 'b' => ['f', 'g'], 'c' => ['h', 'i']}
 
+puts hsh1['b'][1]
+
 hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
+
+puts hsh2[:third].key(0)
