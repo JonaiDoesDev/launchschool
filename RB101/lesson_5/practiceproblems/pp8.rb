c34445a43deba5fb7877491fd7280cecce1485c9
Jonai Soto <jonaisoto.dev@gmail.com>
2023-03-06 20:55:36 -0500

    practice problems

--- RB101/lesson_5/practiceproblems/pp8.rb
+++ RB101/lesson_5/practiceproblems/pp8.rb
@@ -1,7 +1,12 @@
-hsh = {first: ['the', 'quick']}
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+
+vowels = 'aeiou'
 
 hsh.each do |key, value|
-  counter = 0
-  puts value[counter].chars
+  value.each do |string|
+    string.chars.each do |char|
+      puts char if vowels.include?(char)
+    end
+  end
   end
 
