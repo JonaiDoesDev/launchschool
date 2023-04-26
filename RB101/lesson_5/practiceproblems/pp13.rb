33c1a9dbe049fdd5232d73967fe44423acb3574c
Jonai Soto <jonaisoto.dev@gmail.com>
2023-04-26 00:27:49 -0400

    practice problems

--- RB101/lesson_5/practiceproblems/pp13.rb
+++ RB101/lesson_5/practiceproblems/pp13.rb
@@ -1,7 +1,7 @@
 arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
 
-puts arr.select do |elements|
-  elements.sort_by |array|
-    array.odd?
+arr.sort_by do |sub_arr|
+  sub_arr.select do |num|
+  num.even?
 end
 end
