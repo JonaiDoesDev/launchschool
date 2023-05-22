5fc1997cec22609ea8df18d0094c721e52d8270e
Jonai Soto <jonaisoto.dev@gmail.com>
2023-05-22 18:55:50 -0400

    building a UUID code generator

--- RB101/lesson_5/practiceproblems/pp16.rb
+++ RB101/lesson_5/practiceproblems/pp16.rb
@@ -1,14 +1,16 @@
-string_length = 8
-
-string1 = rand(2**string_length).to_s(2)
-string2 = "this is string two"
-string3 = "this is string three"
-string4 = "this is string four"
-string5 = "this is string five"
-
-puts "#{string1}"
 
 
-# def uuid_code()
-#   puts "#{string1}" + "#{string2}" + "#{string3}" + "#{string4}" + "#{string5}"
-# end
+def uuid_code()
+string_length1 = 8
+string_length2 = 4
+string_length3 = 12
+
+string1 = rand(36**string_length1).to_s(36)
+string2 = rand(36**string_length2).to_s(36)
+string3 = rand(36**string_length2).to_s(36)
+string4 = rand(36**string_length2).to_s(36)
+string5 = rand(36**string_length3).to_s(36)
+  puts "#{string1}-" + "#{string2}-" + "#{string3}-" + "#{string4}-" + "#{string5}"
+end
+
+puts uuid_code()
