8d638e5e97410e42f1a419a4f18142afabbb9f2a
Jonai Soto <jonaisoto.dev@gmail.com>
2023-05-22 18:49:27 -0400

    working on some more problems

---
+++ RB101/lesson_5/practiceproblems/pp16.rb
@@ -0,0 +1,14 @@
+string_length = 8
+
+string1 = rand(2**string_length).to_s(2)
+string2 = "this is string two"
+string3 = "this is string three"
+string4 = "this is string four"
+string5 = "this is string five"
+
+puts "#{string1}"
+
+
+# def uuid_code()
+#   puts "#{string1}" + "#{string2}" + "#{string3}" + "#{string4}" + "#{string5}"
+# end
