a5d26c557e6f78de9572b8ccfcd1d23da44f3f85
Jonai Soto <jonaisoto.dev@gmail.com>
2023-07-24 16:21:29 -0400

    working on some problems

--- RB101/lesson_5/practiceproblems/pp16.rb
+++ RB101/lesson_5/practiceproblems/pp16.rb
@@ -1,10 +1,11 @@
 randoms = ('a'..'z').to_a + ('0'..'9').to_a
-key1 = 1
-key2 = 2
-key3 = 3
-key4 = 4
-key5 = 5
+string_length = 8 
+string_length_2 = 4
+string_length_3 = 12
+key1 = (36**(string_length-1) + rand(36**string_length - 36**(string_length-1))).to_s(36)
+key2 = (36**(string_length_2-1) + rand(36**string_length_2 - 36**(string_length_2-1))).to_s(36)
+key3 = (36**(string_length_2-1) + rand(36**string_length_2 - 36**(string_length_2-1))).to_s(36)
+key4 = (36**(string_length_2-1) + rand(36**string_length_2 - 36**(string_length_2-1))).to_s(36)
+key5 = (36**(string_length_3-1) + rand(36**string_length_3 - 36**(string_length_3-1))).to_s(36)
 
-puts master_key = "#{key1}" + "#{key2}" + "#{key3}" + "#{key4}" + "#{key5}"
-
-ok lets continue.
+puts master_key = "#{key1}-" + "#{key2}-" + "#{key3}-" + "#{key4}-" + "#{key5}"
