bf1638de249f217290968724f952f3d129b192db
Jonai Soto <jonaisoto.dev@gmail.com>
2023-07-27 17:43:44 -0400

    working with blocks...and hashes

--- RB101/lesson_5/workingwithblocks/sandbox.rb
+++ RB101/lesson_5/workingwithblocks/sandbox.rb
@@ -1,7 +1,6 @@
-my_arr = [[18, 7], [3, 12]].each do |arr|
-  arr.each do |num|
-    if num > 5
-      puts num
-    end
+[{ a: 'ant', b: 'elephant' }, { c: 'cat' }].select do |hash|
+  hash.any? do |key, value|
+    value[0] == key.to_s
   end
 end
+# => [{ :c => "cat" }]
