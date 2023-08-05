39fe2b59a3138d6265a114620eae78054dd445e3
Jonai Soto <jonaisoto.dev@gmail.com>
2023-08-05 18:26:37 -0400

    working through practice problems a second time

--- RB101/lesson_5/practiceproblems/pp3-1.rb
+++ RB101/lesson_5/practiceproblems/pp3-1.rb
@@ -6,13 +6,21 @@ arr3 = [['abc'], ['def'], {third: ['ghi']}]
 
 hsh1 = {'a' => ['d', 'e'], 'b' => ['f', 'g'], 'c' => ['h', 'i']}
 
-hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
+hsh2 = {
+  first: {'d' => 3}, 
+  second: {'e' => 2, 'f' => 1}, 
+  third: {'g' => 0}
+}
 
 
 
 
-puts arr1[2][1][3]
+# puts arr1[2][1][3]
 
-puts arr2[1][:third][0]
+# puts arr2[1][:third][0]
 
-puts arr3[2][:third][0][0]
+# puts arr3[2][:third][0][0]
+
+# puts hsh1["b"][1]
+
+puts hsh2[:third].key(0)
