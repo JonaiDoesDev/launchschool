2dbc233fd225e854028c4a09d1bbdf5d5599d3d2
Jonai Soto <jonaisoto.dev@gmail.com>
2023-08-09 13:31:55 -0400

    working on some problems

---
+++ RB101/lesson_5/practiceproblems/pp5-1.rb
@@ -0,0 +1,16 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+
+total_male_age = 0
+
+munsters.each_value do |anything|
+  total_male_age += anything["age"] if anything['gender'] == 'male'
+end
+
+puts total_male_age
