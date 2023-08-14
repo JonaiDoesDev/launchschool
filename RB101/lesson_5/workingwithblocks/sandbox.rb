07966483fcf2f76cfd73cd4a11c64b372ba075d9
Jonai Soto <jonaisoto.dev@gmail.com>
2023-08-14 15:02:41 -0400

    syncing folders

--- RB101/lesson_5/workingwithblocks/sandbox.rb
+++ RB101/lesson_5/workingwithblocks/sandbox.rb
@@ -1,6 +1,9 @@
-[{ a: 'ant', b: 'elephant' }, { c: 'cat' }].select do |hash|
-  hash.any? do |key, value|
-    value[0] == key.to_s
+my_arr = [[18, 7], [3, 12]].each do |arr|
+  arr.each do |num|
+    if num > 5
+      puts num
+    end
   end
 end
-# => [{ :c => "cat" }]
+
+ 
