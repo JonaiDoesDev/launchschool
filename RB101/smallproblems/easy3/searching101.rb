62fc4dbb9a3ec9e19f7ccbee0071a8a616ec5fb4
Jonai Soto <jonaisoto.dev@gmail.com>
2023-09-18 12:50:28 -0400

    completing some exercise problems

---
+++ RB101/smallproblems/easy3/searching101.rb
@@ -0,0 +1,33 @@
+numbers = []
+
+
+print 'Enter your first number'
+numbers << gets.chomp
+
+print 'Enter your second number'
+numbers << gets.chomp
+
+print 'Enter your third number'
+numbers << gets.chomp
+
+print 'Enter your fourth number'
+numbers << gets.chomp
+
+print 'Enter your sixth number'
+numbers << gets.chomp
+
+print 'Enter your last number'
+number << gets.chomp
+
+
+
+
+print numbers
+
+
+
+# if numbers.include
+# print numbers
+
+# def ask_six
+#   loop do 
