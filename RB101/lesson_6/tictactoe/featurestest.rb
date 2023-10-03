a81b0fe5abbfcc0cbd35a9e964d3d941ab3e3455
Jonai Soto <jonaisoto.dev@gmail.com>
2023-10-02 20:02:02 -0400

    working on bonus features

--- RB101/lesson_6/tictactoe/featurestest.rb
+++ RB101/lesson_6/tictactoe/featurestest.rb
@@ -1,38 +1,10 @@
 require 'pry-byebug'
 
-#IMPROVE JOIN
-
-# arr = [1, 2, 3, 4, 5]
-# # arr = [1, 2]
-
-
-# # def joinor(brd)
-# #   if brd.length == 1
-# #     puts brd
-# #   else
-# #     brd.insert(-2, "or").join(", ")
-# #   end
-# # end
-
-# #  puts joinor(arr)
-
-
-#  def joinor(arr, delimiter=', ', word='or')
-#   case arr.size
-#   when 0 then ''
-#   when 1 then arr.first.to_s
-#   when 2 then arr.join(" #{word} ")
-#   else
-#     arr[-1] = "#{word} #{arr.last}"
-#     arr.join(delimiter)
-#   end
-#  end
-
-#  joinor(arr)
-
-#KEEP SCORE
-scoreboard = {"Player" => 0, "Computer" => 0}
-case detect_winner()
-  scoreboard["Player"] += 1
-  break if scoreboard["Player"] == 5
+def find_at_risk_square(line, board)
+  binding.pry
+  if board.values_at(*line).count('X') == 2
+    board.select{|k,v| line.include?(k) && v == ' '}.keys.first
+  else
+    nil
+  end
 end
--- RB101/lesson_6/tictactoe/tictactoe.rb
+++ RB101/lesson_6/tictactoe/tictactoe.rb
@@ -3,6 +3,9 @@ require 'pry-byebug'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                  [[1, 5, 9], [3, 5, 7]]
 
 scores = {"Player" => 0, "Computer" => 0}
 
@@ -78,11 +81,8 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
-                  [[1, 5, 9], [3, 5, 7]]
 
-  winning_lines.each do |line|
+  WINNING_LINES.each do |line|
     if brd[line[0]] == PLAYER_MARKER &&
        brd[line[1]] == PLAYER_MARKER &&
        brd[line[2]] == PLAYER_MARKER
@@ -95,6 +95,8 @@ def detect_winner(brd)
   end
   nil
 end
+
+
 
 def display_scoreboard(scores)
   puts "Score: Player: #{scores["Player"]} | Computer: #{scores["Computer"]}"
