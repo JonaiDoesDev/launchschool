9cf809b37935c98638a52e60ed4c815bad6cfe3d
Jonai Soto <jonaisoto.dev@gmail.com>
2023-10-03 19:52:55 -0400

    Building a Twenty-One; creating the initial playing cards data structure

---
+++ RB101/lesson_6/twentyone/twentyone.rb
@@ -0,0 +1,7 @@
+deck_hash = {
+  :hearts => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"],["Jack", "Queen", "King"]],
+  :diamonds => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"], ["Jack", "Queen", "King"]],
+  :clubs => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"], ["Jack", "Queen", "King"]],
+  :spades => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"], ["Jack", "Queen", "King"]]
+}
+
