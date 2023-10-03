12dfdcb9c45b687d77a5b6dbe5664756cde37f19
Jonai Soto <jonaisoto.dev@gmail.com>
2023-10-02 22:27:04 -0400

    few bonus features

--- RB101/smallproblems/easy1/bannerizer.rb
+++ RB101/smallproblems/easy1/bannerizer.rb
@@ -1,15 +1,28 @@
+vowels = ["a", "e", "i", "o", "u"]
+
+message = "sfjkhdsjkfhuefhjdfhjksdhfjkdshfjueuhfjkdsjkfhjfhdsjfhdsjfhuejbn"
+
+def truncate(text)
+  text.size > 25
+  text =  text[0, 20]
+  puts "| #{text} |"
+end
+
 
 def box(text)
-  line_horizontal = "-#{"-" * (text.size * 2)}"
-  line_vertical = "|#{" " * (text.size * 2)}|"
+  vowels = ["a", "e", "i", "o", "u"]
+
+  line_horizontal = "+#{"-" * (text.size + 2)}+"
+  line_vertical = "|#{" " * (text.size + 2)}|"
 
   puts line_horizontal
   puts line_vertical
-  puts "|           #{text}                             |"
+  truncate(text)
   puts line_vertical
   puts line_horizontal
 
 end
 
 
-box("This is what I decided to write in here.")
+
+box("This is a smaller message this is also a longer message")
