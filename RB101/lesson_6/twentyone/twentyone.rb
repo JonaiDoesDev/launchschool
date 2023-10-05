479e8bc9144a36d98e265a22a75652efbb6a29fa
Jonai Soto <jonaisoto.dev@gmail.com>
2023-10-04 23:04:25 -0400

    Added the initial card draw for both the player and dealer.

--- RB101/lesson_6/twentyone/twentyone.rb
+++ RB101/lesson_6/twentyone/twentyone.rb
@@ -1,35 +1,28 @@
-require 'pry-byebug'
-
 deck_of_cards = {
-  :hearts => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"],["Jack", "Queen", "King"]],
-  :diamonds => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"], ["Jack", "Queen", "King"]],
-  :clubs => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"], ["Jack", "Queen", "King"]],
-  :spades => [[2, 3, 4, 5, 6, 7, 8, 9, 10], ["Ace"], ["Jack", "Queen", "King"]]
+  :Hearts => [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"],
+  :Diamonds => [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"],
+  :Clubs => [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"],
+  :Spades => [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"]
 }
 
-player_deck = []
-dealer_deck = []
-
+# player_deck = random_draw(deck_of_cards)
+# dealer_deck = []
 
 def random_draw(deck)
-  decky = {}
-  deck.to_a.sample << decky
-  decky
-  end
-
-
-random_draw(deck_of_cards)
-
-
-# Game Logic
-binding.pry
-loop do
-  deck_of_cards.to_a.sample << player_deck
-  break if player_deck.size == 2
+  suit = deck.keys.sample
+  cards = deck[suit]
+  chosen_card = cards.sample
+  puts "You drew a #{chosen_card} of #{suit}"
 end
 
-puts player_deck
+def view_player_deck(deck)
+  puts "You have: #{deck}"
+end
 
-players_turn
+counter = 0
 
-dealers_turn
+loop do
+  random_draw(deck_of_cards)
+  counter += 1
+  break if counter == 2
+end
