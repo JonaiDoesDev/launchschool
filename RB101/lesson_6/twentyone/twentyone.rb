2bed023181d3e05a628c6ccab233ae909f5d2bf5
Jonai Soto <jonaisoto.dev@gmail.com>
2023-10-05 10:01:28 -0400

    Removed some unnecesary lines and white space

--- RB101/lesson_6/twentyone/twentyone.rb
+++ RB101/lesson_6/twentyone/twentyone.rb
@@ -5,24 +5,24 @@ deck_of_cards = {
   :Spades => [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"]
 }
 
-# player_deck = random_draw(deck_of_cards)
-# dealer_deck = []
+player_deck = []
+dealer_deck = []
 
 def random_draw(deck)
   suit = deck.keys.sample
   cards = deck[suit]
   chosen_card = cards.sample
-  puts "You drew a #{chosen_card} of #{suit}"
 end
 
-def view_player_deck(deck)
-  puts "You have: #{deck}"
+def display_hands(hand1, hand2)
+  puts "The Player's hand: #{hand1}"
+  puts "The dealer's hand: #{hand2}"
 end
-
-counter = 0
 
 loop do
-  random_draw(deck_of_cards)
-  counter += 1
-  break if counter == 2
+  player_deck << random_draw(deck_of_cards)
+  dealer_deck << random_draw(deck_of_cards)
+  break if player_deck.size == 2 && dealer_deck.size == 2
 end
+
+display_hands(player_deck, dealer_deck)
