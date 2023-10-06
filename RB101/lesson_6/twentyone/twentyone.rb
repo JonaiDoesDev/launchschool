a6657180a23f331af09fba5dfca6cf97591733c3
Jonai Soto <jonaisoto.dev@gmail.com>
2023-10-05 22:26:06 -0400

    Converting face card into working numbers

--- RB101/lesson_6/twentyone/twentyone.rb
+++ RB101/lesson_6/twentyone/twentyone.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 deck_of_cards = {
   :Hearts => [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"],
   :Diamonds => [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"],
@@ -14,17 +16,59 @@ def random_draw(deck)
   chosen_card = cards.sample
 end
 
-def display_hands(hand1, hand2)
-  puts "The Player's hand: #{hand1}"
-  puts "The dealer's hand: #{hand2}"
+def display_hands(player, dealer)
+  puts "The Player's hand: #{player}"
+  puts "The dealer's hand: #{dealer}"
 end
+
+def card_value_conversions(player, dealer)
+  new_player_deck = player.map do |card|
+      if card == "Jack"
+        "10".to_i
+      elsif card == "Queen"
+        "10".to_i
+      elsif card == "King"
+        "10".to_i
+      end
+    end
+  player << new_player_deck
+
+  new_dealer_deck = dealer.map do |card|
+      if card == "Jack"
+        "10".to_i
+      elsif card == "Queen"
+        "10".to_i
+      elsif card == "King"
+        "10".to_i
+      end
+  end
+  dealer << new_dealer_deck
+end
+
+
+# def winning_hand(player, dealer)
+#   usuable_card_values_player = card_value_conversions(player)
+#   usuable_card_values_player.sum
+# end
+
+
+# if ace is drawn choose between 1 or 11 to be the value of ace
+# depending on if the total value of the cards respective deck will put it over 21
+#if not, choose 11, if yes, choose 1
+
+# def ace_value(player)
+#   if player.include?("Ace") && game is < 21 
+#   end
+# end
 
 loop do
   player_deck << random_draw(deck_of_cards)
   dealer_deck << random_draw(deck_of_cards)
-  break if player_deck.size == 2 && dealer_deck.size == 2
+  break if dealer_deck.size == 2
 end
 
+card_value_conversions(player_deck, dealer_deck)
 display_hands(player_deck, dealer_deck)
 
-Test
+# display_hands(player_deck, dealer_deck)
+# winning_hand(player_deck, dealer_deck)
