2dfbd66f591ef670d9f1dc48129bf96360f4feec
Jonai Soto <jonaisoto.dev@gmail.com>
2023-10-26 20:30:46 -0400

    easy problems

--- RB101/smallproblems/easy3/searching101.rb
+++ RB101/smallproblems/easy3/searching101.rb
@@ -1,33 +1,34 @@
 numbers = []
 
-
 print 'Enter your first number'
-numbers << gets.chomp
+numbers << gets.chomp.to_i
 
 print 'Enter your second number'
-numbers << gets.chomp
+numbers << gets.chomp.to_i
 
 print 'Enter your third number'
-numbers << gets.chomp
+numbers << gets.chomp.to_i
 
 print 'Enter your fourth number'
-numbers << gets.chomp
+numbers << gets.chomp.to_i
 
-print 'Enter your sixth number'
-numbers << gets.chomp
+print 'Enter your fifth number'
+numbers << gets.chomp.to_i
 
 print 'Enter your last number'
-number << gets.chomp
+sixth_number = gets.chomp.to_i
+
+same_numbers = numbers.select do |n|
+ n == sixth_number
+end
+
+p numbers
+p same_numbers
+p sixth_number
 
 
-
-
-print numbers
-
-
-
-# if numbers.include
-# print numbers
-
-# def ask_six
-#   loop do 
+ if same_numbers.include?(sixth_number)
+ puts "Your last number #{sixth_number} is part of #{numbers}"
+else
+  puts "Your last number #{sixth_number} is not part of #{numbers}"
+end
