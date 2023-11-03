b518e67a9157031f221e2faf09866af42bf78caa
Jonai Soto <jonaisoto.dev@gmail.com>
2023-11-02 22:30:52 -0400

    more small problems

---
+++ RB101/smallproblems/easy3/uppercase.rb
@@ -0,0 +1,9 @@
+def uppercase?(string)
+  if string == string.upcase
+    true
+  else
+    false
+end
+end
+
+uppercase?("jonai")
--- RB109d/spotwiki/sandbox.rb
+++
@@ -1,9 +0,0 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-a.each do |number|
-  evaluated = number + 1
-end
-
-puts evaluated
-
