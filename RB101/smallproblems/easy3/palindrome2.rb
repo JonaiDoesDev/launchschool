707a9a328b3ee9cdee501b10403115ad6b6a85e0
Jonai Soto <jonaisoto.dev@gmail.com>
2023-11-07 15:57:46 -0500

    working on some problems

--- RB101/smallproblems/easy3/palindrome2.rb
+++ RB101/smallproblems/easy3/palindrome2.rb
@@ -1,15 +1,19 @@
 def palindrome?(string)
-  deleted_characters = [",", "'", " "]
-  string = string.downcase.delete(deleted_characters.join(","))
-  string
   backwards = string.reverse
   string == backwards
 end
 
+def palindrome2?(string)
+  deleted_characters = [",", "'", " "]
+  string = string.downcase.delete(deleted_characters.join(","))
+  palindrome?(string)
+end
+
+
 word = "Racecar"
 another = "Madam, I'm Adam"
 
-puts palindrome?(word)
-puts palindrome?(another)
+puts palindrome2?(word)
+puts palindrome2?(another)
 
 
