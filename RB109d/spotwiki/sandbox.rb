69dca6307efc27b358f91407fe9cf13f5f4c648c
Jonai Soto <jonaisoto.dev@gmail.com>
2023-11-13 22:13:46 -0500

    working on some small problems

--- RB109d/spotwiki/sandbox.rb
+++ RB109d/spotwiki/sandbox.rb
@@ -1,10 +1,7 @@
-animal = "dog"
-
-loop do |_|
-  animal = "cat"
-  var = "ball"
-  break
+diff = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |arr|
+  arr.each do |key, value|
+    value + 1
+end
 end
 
-puts animal
-puts var
+puts diff
