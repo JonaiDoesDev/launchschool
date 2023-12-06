9609180911fee52cadfca280dfa4a946fd8dc650
Jonai Soto <jonaisoto.dev@gmail.com>
2023-12-05 21:27:39 -0500

    working on some problems

--- RB109d/spotwiki/sandbox.rb
+++ RB109d/spotwiki/sandbox.rb
@@ -1,6 +1,20 @@
-flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+competitors = [{name: "Gracie-Leigh Cruz",      height: "5ft 2in",  weight: 129},
+               {name: "Suhail Stott",           height: "5ft 7in",  weight: 169},
+               {name: "Ava-May Hall",           height: "5ft 7in",  weight: 177},
+               {name: "Katie-Louise Milne",     height: "5ft 11in", weight: 122},
+               {name: "Akbar Bolton",           height: "5ft 4in",  weight: 135},
+               {name: "Joy Thompson",           height: "5ft 10in", weight: 177},
+               {name: "Rickie Nunez",           height: "5th 5in",  weight: 142},
+               {name: "Yaqub Combs",            height: "6ft 0in",  weight: 193},
+               {name: "Tulisa Guzman",          height: "5ft 3in",  weight: 111},
+               {name: "Haydn Farrell",          height: "5ft 7in",  weight: 155}]
 
-hash = {}
-flintstones.each_with_index do |element, index|
-  hash[element] = index
+               
+
+competitors_id = []
+competitors.map do |key, value|
+  p key[0,3]
 end
+
+competitors_id
+
