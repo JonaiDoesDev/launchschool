12578675307475f9ec283091700d4e4441e8c1ef
Jonai Soto <jonaisoto.dev@gmail.com>
2023-12-11 19:20:50 -0500

    working on some problems

--- RB109d/spotwiki/sandbox.rb
+++ RB109d/spotwiki/sandbox.rb
@@ -1,20 +1,3 @@
-competitors = [{name: "Gracie-Leigh Cruz",      height: "5ft 2in",  weight: 129},
-               {name: "Suhail Stott",           height: "5ft 7in",  weight: 169},
-               {name: "Ava-May Hall",           height: "5ft 7in",  weight: 177},
-               {name: "Katie-Louise Milne",     height: "5ft 11in", weight: 122},
-               {name: "Akbar Bolton",           height: "5ft 4in",  weight: 135},
-               {name: "Joy Thompson",           height: "5ft 10in", weight: 177},
-               {name: "Rickie Nunez",           height: "5th 5in",  weight: 142},
-               {name: "Yaqub Combs",            height: "6ft 0in",  weight: 193},
-               {name: "Tulisa Guzman",          height: "5ft 3in",  weight: 111},
-               {name: "Haydn Farrell",          height: "5ft 7in",  weight: 155}]
+todo_lists = [{id: 1, list_name: 'Groceries', todos: [{ id: 1, name: 'Bread', completed: false }, { id: 2, name: 'Milk', completed: false }, { id: 3, name: 'Apple Juice', completed: false }]}]
 
-               
-
-competitors_id = []
-competitors.map do |key, value|
-  p key[0,3]
-end
-
-competitors_id
-
+todo_lists[todos][2][name] = "Orange Juice"
