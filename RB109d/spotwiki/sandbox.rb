127c762bae32e04d572fb2b4e8619547979719d1
Jonai Soto <jonaisoto.dev@gmail.com>
2024-01-08 21:30:46 -0500

    working on some problems

--- RB109d/spotwiki/sandbox.rb
+++
@@ -1,6 +0,0 @@
-todo_lists = [{id: 1, list_name: 'Groceries', todos: [{ id: 1, name: 'Bread', completed: false }, { id: 2, name: 'Milk', completed: false }, { id: 3, name: 'Apple Juice', completed: false }]}]
-
-todo_lists[todos][2][name] = "Orange Juice"
-
-another test
-testdf 
