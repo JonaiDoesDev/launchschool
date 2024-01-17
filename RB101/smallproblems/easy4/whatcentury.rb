8f50780130694b11d6b21d2f1fcf926621da100e
Jonai Soto <jonaisoto.dev@gmail.com>
2024-01-16 20:04:27 -0500

    doing some small problems

--- RB101/smallproblems/easy4/whatcentury.rb
+++ RB101/smallproblems/easy4/whatcentury.rb
@@ -1,6 +1,17 @@
-def century?(year)
-  year = year.to_s
-  puts year.end_with?("01") 
+def century(year)
+  century = year / 100 + 1
+  century -= 1 if year % 100 == 0
 end
 
-puts century?(2001)
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
+  end
+end
+
