b9ea90e8282b2057562a2b9ebb175c015ad6d676
JonaiDoesDev <jonaisoto.dev@gmail.com>
2024-01-25 14:50:46 -0500

    working on small problems

--- RB101/smallproblems/easy4/convertstringtonumber.rb
+++ RB101/smallproblems/easy4/convertstringtonumber.rb
@@ -1,18 +1,13 @@
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
 def string_to_integer(string)
-  numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  holder_array = []
-  split_elements = string.chars
-  split_elements.each do |element|
-    if element == numbers.to_a.any?
-      holder_array << element
-    end
-  end
-    puts holder_array.join
+numbers = {'0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9 }
+
+digits = []
+string.chars.map do |char|
+  digits << numbers[char]
+end
+puts digits.join
 end
 
 
-string_to_integer('1234')
-# string_to_integer('4321') == 4321
-# string_to_integer('570') == 570
+
+string_to_integer('570')
